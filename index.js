cefc711f3ac383836b2b78bf5a1e2abb86e2d67d
Lucas Lu <lucaslu@hotmail.com>
2022-11-21 15:38:26 -0800

    Initial Folder Structure and Git Repo

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("node:path");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 5500;
+
+app.get("/", (_request, response) => {
+  response.status(200).sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT} 🚀`);
+});

30bc832826cd52df37178333eede131519fd37dd
Miguel Lopez <lopezpedres@gmail.com>
2022-11-23 14:06:30 -0700

    Extra fields needed

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -5,7 +5,7 @@ const knex = require("knex")(require("../knexfile"));
 exports.index = (_req, res) => {
   knex
     .from("warehouses")
-    .select("id", "warehouse_name", "contact_name")
+    .select("id", "warehouse_name", "contact_name", "address", "contact_phone","contact_email","city","country")
     .then((data) => {
       res.status(200).json(data);
     })

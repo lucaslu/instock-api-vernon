1feafd7d97f5723836f0d16a587188219d3c74b2
Miguel Lopez <lopezpedres@gmail.com>
2022-11-23 15:21:19 -0700

    Added query to get single inventory item

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -9,3 +9,20 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Inventories: ${err}`)
     );
 };
+exports.singleInventoryItem = (req, res) => {
+  knex("inventories")
+    .where({ id: req.params.id })
+    .then((data) => {
+      if (!data.length) {
+        return res
+          .status(404)
+          .send(`Inventory with id: ${req.params.id} is not found`);
+      }
+      res.status(200).json(data[0]);
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error retrieving Inventorie ${req.params.id} ${err}`)
+    );
+};

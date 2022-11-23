200a3f05683960ce7307b9f132ea22aa80297096
Miguel Lopez <lopezpedres@gmail.com>
2022-11-23 15:12:24 -0700

    Get all inventory Items

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,6 +2,15 @@ const knex = require("knex")(require("../knexfile"));
 
 exports.index = (_req, res) => {
   knex("inventories")
+    .join("warehouses", "warehouses.id", "=", "inventories.warehouse_id")
+    .select(
+      "warehouses.warehouse_name",
+      "inventories.id",
+      "inventories.item_name",
+      "inventories.category",
+      "inventories.status",
+      "inventories.quantity"
+    )
     .then((data) => {
       res.status(200).json(data);
     })

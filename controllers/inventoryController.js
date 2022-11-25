68dd777a69fdeb52a2b1cfef22e43d44784072e9
Ian DC <jokerzamba@hotmail.com>
2022-11-25 15:20:11 -0800

    Add Inventory Item Patch functionality

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -1,13 +1,15 @@
-const knex = require("knex")(require("../knexfile"));
+const { v4: uuidv4 } = require("uuid");
 
+const knex = require("knex")(require("../knexfile"));
 exports.index = (_req, res) => {
   knex("inventories")
     .join("warehouses", "warehouses.id", "=", "inventories.warehouse_id")
     .select(
       "warehouses.warehouse_name",
+      "inventories.warehouse_id",
       "inventories.id",
-      "inventories.item_name",
       "inventories.category",
+      "inventories.item_name",
       "inventories.status",
       "inventories.quantity"
     )
@@ -18,6 +20,7 @@ exports.index = (_req, res) => {
       res.status(400).send(`Error retrieving Inventories: ${err}`)
     );
 };
+
 exports.singleInventoryItem = (req, res) => {
   knex("inventories")
     .where({ id: req.params.id })
@@ -36,26 +39,107 @@ exports.singleInventoryItem = (req, res) => {
     );
 };
 
+// /** Add a new Inventory Item */
+
+// exports.addInventoryItem = (req, res) => {
+//   // Validate the request body for required data
+//   if (
+//     !req.body.warehouse_name ||
+//     // !req.body.warehouse_id ||
+//     !req.body.item_name ||
+//     !req.body.description ||
+//     !req.body.category ||
+//     !req.body.status ||
+//     !req.body.quantity
+//   ) {
+//     return res
+//       .status(400)
+//       .send(
+//         "Please make sure to provide warehouse_name, item_name, description, category, status, and quantity"
+//       );
+//   }
+
+//   const newInventoryId = uuidv4();
+//   knex("inventories")
+//     .insert({ ...req.body, id: newInventoryId })
+//     .then((_data) => {
+//       knex("inventories")
+//         .where({ id: newInventoryId })
+//         .then((data) => {
+//           res.status(201).json(data[0]);
+//         });
+//     })
+//     .catch((err) =>
+//       res.status(400).send(`Error creating Inventory Item: ${err}`)
+//     );
+// };
+
+/** Put request - update a single iventory item based on a single passed in ID */
+exports.updateInventoryItem = (req, res) => {
+  knex("inventories")
+    .update({
+      warehouse_name: req.body.warehouse_name || "",
+      warehouse_id: req.body.warehouse_id || "",
+      item_name: req.body.item_name || "",
+      description: req.body.description || "",
+      category: req.body.category || "",
+      status: req.body.status || "",
+      quantity: req.body.quantity || "",
+    })
+    .where({ id: req.params.id })
+    .then((_data) => {
+      knex("inventories")
+        .where({ id: req.params.id })
+        .then((data) => {
+          res.status(200).json(data[0]);
+        });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error updating Inventory Item ${req.params.id} ${err}`)
+    );
+};
+
+/**
+ * INSERT PATCH end point and route!
+ */
+exports.patchInventoryItem = (req, res) => {
+  knex("inventories")
+    .update(req.body)
+    .where({ id: req.params.id })
+    .then((_data) => {
+      knex("inventories")
+        .where({ id: req.params.id })
+        .then((data) => {
+          res.status(200).json(data[0]);
+        });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error updating Inventory Item ${req.params.id} ${err}`)
+    );
+};
+
 exports.deleteItem = (req, res) => {
   knex("inventories")
     .where({ id: req.params.id })
     .delete()
     .then(() => {
       knex("inventories")
-      .join("warehouses", "warehouses.id", "=", "inventories.warehouse_id")
-      .select(
-        "warehouses.warehouse_name",
-        "inventories.id",
-        "inventories.item_name",
-        "inventories.category",
-        "inventories.status",
-        "inventories.quantity"
-      )
-      .then((data)=>{
-        res
-          .status(200)
-          .json(data);
-      })
+        .join("warehouses", "warehouses.id", "=", "inventories.warehouse_id")
+        .select(
+          "warehouses.warehouse_name",
+          "inventories.id",
+          "inventories.item_name",
+          "inventories.category",
+          "inventories.status",
+          "inventories.quantity"
+        )
+        .then((data) => {
+          res.status(200).json(data);
+        });
     })
     .catch((err) =>
       res.status(400).send(`Error deleting item ${req.params.id} ${err}`)
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,9 +2,12 @@ const router = require("express").Router();
 const inventoryController = require("../controllers/inventoryController");
 
 router.route("/").get(inventoryController.index);
+// .post(inventoryController.addInventoryItem);
 router
   .route("/:id")
   .get(inventoryController.singleInventoryItem)
+  .put(inventoryController.updateInventoryItem)
+  .patch(inventoryController.patchInventoryItem)
   .delete(inventoryController.deleteItem);
 
 module.exports = router;

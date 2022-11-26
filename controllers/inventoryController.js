437cd491e8d589b2c053491b2e6375b3621854b6
Ian DC <jokerzamba@hotmail.com>
2022-11-25 16:32:30 -0800

    Update from features branch

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -25,7 +25,7 @@ exports.singleInventoryItem = (req, res) => {
   knex("inventories")
     .where({ id: req.params.id })
     .then((data) => {
-      if (data.length) {
+      if (!data.length) {
         return res
           .status(404)
           .send(`Inventory with id: ${req.params.id} is not found`);
@@ -43,7 +43,6 @@ exports.singleInventoryItem = (req, res) => {
 
 exports.addInventoryItem = (req, res) => {
   // Validate the request body for required data
-  console.log(req.body);
   if (
     req.body.warehouse_id === "" ||
     req.body.item_name === "" ||
@@ -70,6 +69,24 @@ exports.addInventoryItem = (req, res) => {
         });
     })
     .catch((err) => res.status(400).send(`Error creating Inventory: ${err}`));
+};
+
+exports.patchInventoryItem = (req, res) => {
+  knex("inventories")
+    .update(req.body)
+    .where({ id: req.params.id })
+    .then((_data) => {
+      knex("inventories")
+        .where({ id: req.params.id })
+        .then((data) => {
+          res.status(200).json(data[0]);
+        });
+    })
+    .catch((err) =>
+      res
+        .status(400)
+        .send(`Error updating inventory item ${req.params.id} ${err}`)
+    );
 };
 
 exports.deleteItem = (req, res) => {
